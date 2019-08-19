b11a12870eab8a50be9f76dd21a2a987910d78e8
Abolfazl Reazvani Naraqi <returnAmor@Gmail.com>
2019-08-19 22:12:25 +0430

    Update store.js

--- src/store.js
+++ src/store.js
@@ -5,8 +5,8 @@ import thunk from 'redux-thunk';
 const middleWare = [thunk];
 const store = createStore( rootReducer ,
     compose(
-        applyMiddleware(...middleWare) , 
-        (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) 
+        // applyMiddleware(...middleWare) , (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) 
+        applyMiddleware(...middleWare) ,null)
     ) 
 );
 
